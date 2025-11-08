3cd06c9b7fce95651051d5fc173599b383b67783
Kael717 <jevbautro98@gmail.com>
2025-11-08 15:02:06 +0800

    Remove security headers implementation - Removed SecureHeaders middleware, security config, and Apache security headers from .htaccess

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\SecureHeaders::class, // Security headers for all requests
     ];
 
     protected $middlewareGroups = [
